4b411a42df9181a8ec968bf98e119939ed28f443
Wojciech Skirło <w.skirlo@ideo.pl>
2024-04-06 15:00:14 +0200

    feat: add version

--- app/(app)/(tabs)/account.tsx
+++ app/(app)/(tabs)/account.tsx
@@ -2,6 +2,7 @@ import { Link, useRouter } from 'expo-router';
 import { useAuth } from '@context/auth';
 import { StyleSheet, View, Text } from 'react-native';
 import { Colors } from '@helpers/colors';
+import Constants from 'expo-constants';
 import User from '@components/UI/User';
 import Label from '@components/UI/Label';
 import UIButton from '@components/UI/Button';
@@ -9,6 +10,7 @@ import UIButton from '@components/UI/Button';
 export default function Account() {
   const { signOut, user } = useAuth();
   const router = useRouter();
+  const version = Constants.expoConfig?.version;
 
   const handleSignOut = () => {
     signOut();
@@ -21,10 +23,12 @@ export default function Account() {
         <Label>Profile</Label>
         <User user={user?.username ?? ''} text={user?.email ?? ''} />
       </View>
-      <View style={styles.group}>
-        <Label>Version</Label>
-        <Text style={styles.version}>0.0.1</Text>
-      </View>
+      {version ? (
+        <View style={styles.group}>
+          <Label>Version</Label>
+          <Text style={styles.version}>{version}</Text>
+        </View>
+      ) : null}
       <UIButton text="Sign Out" onPress={handleSignOut} />
 
       <Link href="/sign-in">Test</Link>
